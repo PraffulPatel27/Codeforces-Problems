ab888be0435517394bdbac78cdadd4d95eb00fc8
PraffulPatel27 <prafful123patel@gmail.com>
2024-02-24 00:26:16 +0530

    Educational Codeforces Round 162 (Rated for Div. 2)

---
+++ .history/Monsters_Attack!_20240223214024.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast_read_input_output() ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define mod 1000000007
+#define vec vector<int>
+#define vecl vector<long long>
+#define e "\n"
+#define en endl
+#define pb push_back
+#define ep emplace_back
+#define ist insert
+#define mp make_pair
+#define forf for (auto i=0; i<n; i++)
+#define fore for (auto ch: s)
+int main()
+{
+    fast_read_input_output()
+    ll t;
+    cin>>t;
+
+    while (t--) {
+        ll n,k;
+        cin>>n>>k;
+
+        vecl v(n), p(n);
+        forf  cin>>v[i];
+        vector<int> position(n+1, 0);
+
+        for (int i=0; i<n; i++) {
+            cin>>p[i];
+            position[abs(p[i])] += v[i];
+        }
+        int power = k;
+
+        bool flag= false;
+        for (int i=1; i<=n; i++) {
+            if (power < position[i]) {
+                cout << "NO" << e;
+                flag= true;
+                break;
+            }
+
+            power-= position[i];
+            power+= k;
+        }
+    }
+
+    cout << "YES" << e;
+
+    return 0;
+}
